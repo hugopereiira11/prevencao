4d8951058ec0c47152ae11a434e1041b285ec565
hugopereiira11 <hugopereiira11@gmail.com>
2023-12-29 12:25:16 -0300

    desbugando o menu flutuante e criando o menu js

---
+++ scripts/menu.js
@@ -0,0 +1,26 @@
+const body = document.body
+
+body.innerHTML += `
+<div id="float-menu" style="display: none;">
+<nav>
+  <a href="#">Área de Trocas</a>
+  <a href="#">Avarias</a>
+  <a href="#">Consumo</a>
+  <a href="/nav/orcamentos/orcamentos.html">Orçamentos</a>
+  <a href="#">Perdas</a>
+  <a href="#">Sobre</a>
+</nav>
+</div>
+
+<div id="menu-button">
+<input type="checkbox" name="botao-menu" id="botao-menu">
+<label for="botao-menu">
+    <span id="btn-menu" class="material-icons md-48">
+        menu
+    </span>
+    <span class="material-icons md-48" id="btn-close">
+        close
+    </span>
+</label>
+</div>
+`

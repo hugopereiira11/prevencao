a3f2af09d77baab3f89115c4aba94e57c67886fd
hugopereiira11 <hugopereiira11@gmail.com>
2023-12-29 11:55:28 -0300

    estruturando orcamentos

---
+++ nav/orcamentos/app.js
@@ -0,0 +1,29 @@
+const main = document.querySelector("main");
+const orcamentos = document.querySelector("#orcamentos");
+
+fetch("dados.json")
+  .then((resp) => resp.json())
+  .then((dados) => {
+    console.log(dados)
+
+    let data = dados.sort((a, b) => {
+      if (a.fornecedor < b.fornecedor) {
+        return -1;
+      }
+    });
+
+    data.map((orc) => {
+      main.innerHTML += `
+            <div class="content">
+                <div class="info">
+                    <div class="fornecedor">${orc.fornecedor}</div>
+                    <div class="numero">${orc.num}</div>
+                    <div class="loja">${orc.loja}</div>
+                    <div class="valor">${orc.valor}</div>
+                    <div class="data">${orc.data}</div>
+                    <div class="estado">${orc.estado}</div>
+                </div>
+                <div class="view-pdf"><a href="${orc.link}">Visualizar</a></div>
+            </div>`;
+    });
+  });
